0d077ba1b5bb0154212be7c8ca04735dfd059c3c
EgimenoS <ergisan@gmail.com>
2020-11-14 01:49:01 +0100

    implemented some navbar styles and content. Added responsive behavior for menu items and hamburger button

--- src/app/core/layout/navbar/navbar.component.ts
+++ src/app/core/layout/navbar/navbar.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  showMenu: boolean;
+  width: number;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onResize(): void {
+    this.width = window.innerWidth;
+    this.showMenu = this.width <= 768 ? false : true;
   }
 
+  toggleMenuVisibility(): void {
+    this.showMenu = !this.showMenu;
+  }
+
+  ngOnInit(): void {
+    this.width = window.innerWidth;
+    this.showMenu = this.width <= 768 ? false : true;
+  }
 }
