376b991cb33587449e8bc6e01a5c5f38e013f2ff
EgimenoS <ergisan@gmail.com>
2020-11-29 16:18:30 +0100

    added styles for chat

--- src/app/features/travel-detail/travel-detail.module.ts
+++ src/app/features/travel-detail/travel-detail.module.ts
@@ -5,9 +5,10 @@ import { TravelDetailRoutingModule } from './travel-detail-routing.module';
 import { TravelDetailComponent } from './travel-detail.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { TravelInfoComponent } from './travel-info/travel-info.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
-  declarations: [TravelDetailComponent, TravelInfoComponent],
+  declarations: [TravelDetailComponent, TravelInfoComponent, ChatComponent],
   imports: [CommonModule, SharedModule, TravelDetailRoutingModule, MatChipsModule],
   exports: [MatChipsModule],
 })
