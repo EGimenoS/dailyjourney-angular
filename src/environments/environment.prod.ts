ee582dd77e03ba71e7e5d1369b4a09699d2c2b07
EgimenoS <ergisan@gmail.com>
2020-11-29 02:06:09 +0100

    typo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://dailyjourney-api.herokuapp.com',
-  authUrl: 'https://dailyjourney-api.herokuapp.com',
+  baseUrl: 'https://dailyjourney-api.herokuapp.com/api/v1',
+  authUrl: 'https://dailyjourney-api.herokuapp.com/users',
   baseDom: 'dailyjourney-api.herokuapp.com',
 };
