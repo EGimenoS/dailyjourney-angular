984f0b49e996ef2741523a9d355846659304426c
EgimenoS <ergisan@gmail.com>
2020-11-29 01:40:47 +0100

    changed hover effect on travel cards

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://dailyjourney-api.herokuapp.com/',
+  authUrl: 'https://dailyjourney-api.herokuapp.com/',
+  baseDom: 'dailyjourney-api.herokuapp.com',
 };
