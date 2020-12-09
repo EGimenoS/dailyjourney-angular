7da9452d308a794b88319351f891d3e03380bbec
EgimenoS <ergisan@gmail.com>
2020-12-10 00:12:13 +0100

    fixed some styles

--- src/app/core/components/login/login.component.ts
+++ src/app/core/components/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
-import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-login',
