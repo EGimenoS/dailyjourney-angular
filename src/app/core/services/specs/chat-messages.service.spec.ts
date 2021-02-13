89f3ef83bc1be5f9db17b4e1a8a37d0e38cfcb8a
EgimenoS <ergisan@gmail.com>
2021-02-13 22:09:54 +0100

    added test for create chat message service method

--- src/app/core/services/specs/chat-messages.service.spec.ts
+++ src/app/core/services/specs/chat-messages.service.spec.ts
@@ -4,7 +4,7 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 import { ErrorsService } from '../errors.service';
 import { ChatMessage } from '../../interfaces/chat-message';
 import { endpoint } from 'config';
-import { chatMessages } from 'src/app/mock-data/chat-messages.data';
+import { chatMessageCreateResponse, chatMessages } from 'src/app/mock-data/chat-messages.data';
 
 describe('ChatMessagesService', () => {
   let chatMessagesService: ChatMessagesService;
@@ -32,5 +32,29 @@ describe('ChatMessagesService', () => {
     });
     const getRequest = httpTestingController.expectOne(`${url}?travel_id=1`);
     getRequest.flush(chatMessages);
+  });
+
+  it('should save a new chat message...', () => {
+    chatMessagesService.createNewChatMessage('1', 'hola').subscribe((response) => {
+      expect(response.message).toBe('hola');
+    });
+    const postRequest = httpTestingController.expectOne(url);
+    expect(postRequest.request.method).toEqual('POST');
+    expect(postRequest.request.body.message).toEqual('hola');
+    postRequest.flush(chatMessageCreateResponse);
+  });
+
+  it('should catch an error on saving a new message and call the handleError function', () => {
+    chatMessagesService.createNewChatMessage('1', 'hola').subscribe((response) => {
+      expect(errorsServiceSpy.handleError).toHaveBeenCalled();
+      expect(response).toBe(null);
+    });
+
+    const req = httpTestingController.expectOne(url);
+    expect(req.request.method).toEqual('POST');
+    req.flush('Chat Message create operation failed', {
+      status: 500,
+      statusText: 'Internal Server Error',
+    });
   });
 });
